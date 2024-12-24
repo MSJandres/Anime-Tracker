1245ca23769c581187589395cb81336401152804
krana <kranat10@gmail.com>
2024-12-23 20:07:31 -0600

    fixed all build errors, frontend build properly now

--- client/src/components/AnimeCard.tsx
+++ client/src/components/AnimeCard.tsx
@@ -2,7 +2,6 @@ function AnimeCard({anime}) {
   return (
     <article className="anime-card">
         <a href={anime.url}
-            target='_blank'
             rel='noreferrer'>
             <figure>
                 <img src='{anime.image_url'
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import './style/index.css';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root')!).render(
+ <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
+ </React.StrictMode>
 )
