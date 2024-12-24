f18a23ab360248ea882ddf09601ee28d99fedb91
krana <kranat10@gmail.com>
2024-12-23 19:49:38 -0600

    removed frontend/ folder

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import './style/index.css';
-import App from './App.tsx';
-import './style/App.css'
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!).render(
- <React.StrictMode>
-  <BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
- </React.StrictMode>
+  </StrictMode>,
 )
